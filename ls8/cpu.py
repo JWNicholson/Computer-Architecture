6e7df565b59f974f922cf8d44e96397fc586624f
JWNicholson <john.nicholson812@gmail.com>
2020-08-17 17:16:27 -0400

    refactored ram_read,ram_write by adding mar,mdr. wrote load function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,32 +12,47 @@ class CPU:
         self.pc = 0
         self.sp = 7
 
-    def ram_read(self,value, address):
-        self.ram[address] = value
+    #mar and mdr for read and write functions
+    def ram_read(self,mar):
+        self.ram[mar] 
 
-    def ram_write(self, address):
-        return self.ram[address]
+    def ram_write(self, mar, mdr):
+        return self.ram[mdr]
 
-    def load(self,program):
+    def load(self,prog):
         """Load a program into memory."""
 
         address = 0
 
+        with open(progr) as program:
+            for instruction in program:
+                instruction_split = instruction.split('#')
+                instruction_value = instruction_split[0].strip()#remove spaces at begining and end
+
+                print(f"Instruction Value - {instruction_value}")
+
+                if instruction_value == '':
+                    continue
+                instruction_num = int(instruction_value,2)
+                print(f"To RAM - {instruction_num,address}")
+                self.ram_write(address, instruction_num)
+                address += 1
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
